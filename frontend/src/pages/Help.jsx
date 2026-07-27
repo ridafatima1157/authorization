e272ef23aaeb2d0ba6aa282c688a87d6960ffccd
ridafatima1157 <fatima.rida64000@gmail.com>
2026-07-27 06:55:56 +0500

    fix

--- frontend/src/pages/Help.jsx
+++ frontend/src/pages/Help.jsx
@@ -2,10 +2,7 @@ function Help() {
   return (
     <div>
       <h1>Help Page-again-ok-bye</h1>
-      <h1>Help Page-again-ok-bye</h1>
-      <h1>Help Page-again-ok-bye</h1>
-      <h1>Help Page-again-ok-bye</h1>
-      <h1>Help Page-again-ok-bye</h1>
+  
     </div>
   )
 }
